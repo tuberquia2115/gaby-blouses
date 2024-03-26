c9f0125e4aa383eb32515eb8b6dd6ee96846a26b
Jose Manuel Tuberquia <tuberquia2115@gmail.com>
2024-03-26 13:40:18 -0500

    feat: Add database schema, tables, and relationships for products and categories
    Add database schema, tables, and relationships for products, categories, and product images. This enables the storage of product and category information, including size, gender, and images. It also sets up foreign key constraints for maintaining data integrity. The changes lay the foundation for managing and querying product and category data in the application. No issue references.

---
+++ prisma/migrations/20240323040217_product_category/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `category` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_categoryId_fkey";
+
+-- DropTable
+DROP TABLE "category";
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
